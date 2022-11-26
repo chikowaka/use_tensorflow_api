cb6aba1dbfe4d97968d845ff99f31cdcd311f455
chikowaka <45866740+chikowaka@users.noreply.github.com>
2022-11-26 12:59:25 +0900

    post

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import tensorflow_hub as hub
 import tensorflow_text
 import numpy
-
+from typing import List
 
 # for avoiding error
 import ssl
@@ -13,9 +14,13 @@ app = FastAPI()
 # embed = hub.load("/var/folders/25/z38s61zn0dnbmjcs68r62ywc0000gn/T/tfhub_modules")
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual/3")
 
+class PongModel(BaseModel):
+    text: List[str]
+
 @app.post("/ping")
-def pong(req):
-    texts = ["It is cool", "Is it cool", "It is cool", "I saw a comedy show last night.", "Yesterday, I went to the park."]
+def pong(req: PongModel):
+    print(req.text)
+    texts = req.text
     vectors = embed(texts)
     result = vectors.numpy().tolist()
     print(type(vectors), result)
